05b764bb6967012b8f44caf6b4d76dc2bbcb436d
Samuel Neaves <sn0550@eduroam-199-153.nomadic.bris.ac.uk>
2023-02-17 11:29:34 +0000

    Add optimised doc writer

---
+++ helper_file/doc_writer_optimised.py
@@ -0,0 +1,41 @@
+import os
+import yaml
+import hashlib
+import uuid
+from multiprocessing import Pool, cpu_count
+
+def process_file(file):
+    file_path = os.path.join(root, file)
+    file_size = os.path.getsize(file_path)
+    with open(file_path, 'rb') as f:
+        file_md5 = hashlib.md5()
+        while chunk := f.read(4096):
+            file_md5.update(chunk)
+        file_md5 = file_md5.hexdigest()
+    return {
+        'id': str(uuid.uuid4()),
+        'filename': file,
+        'file_size': file_size,
+        'md5sum': file_md5,
+        'belongs_to_container': "alspacdcs:74fcdd8a-fec5-4ef2-9dd1-3bc16b418640"
+    }
+
+def get_file_info(directory):
+    file_info = []
+    count = 0
+    total_files = sum([len(files) for r, d, files in os.walk(directory)])
+    with Pool(processes=cpu_count()) as pool:
+        for root, dirs, files in os.walk(directory):
+            results = pool.map(process_file, files)
+            file_info.extend(results)
+            count += len(results)
+            print(f'Processed {count} of {total_files} files')
+    return file_info
+
+def write_to_yaml(file_info, yaml_file):
+    with open(yaml_file, 'w', encoding='utf-8') as f:
+        yaml.dump(file_info, f, default_flow_style=False, allow_unicode=True)
+
+directory = '/mnt/storage/private/alspacdata/datasets/dataset_wgs_hiseq_g1/freeze/scripts/'
+file_info = get_file_info(directory)
+write_to_yaml(file_info, 'file_info_scripts.yaml')
