7585e3895fc5bacba8cc7e31801a6cfbab2d60c0
Sam Neaves <sam.neaves@gmail.com>
2023-03-02 13:34:25 +0000

    Update for distribution and file yaml

--- helper_file/doc_writer_optimised.py
+++ helper_file/doc_writer_optimised.py
@@ -4,6 +4,28 @@ import hashlib
 import uuid
 from multiprocessing import Pool, cpu_count, Manager
 
+# def process_file(root, file, progress_queue):
+#     file_path = os.path.join(root, file)
+#     file_size = os.path.getsize(file_path)
+#     with open(file_path, 'rb') as f:
+#         file_md5 = hashlib.md5()
+#         while True:
+#             chunk = f.read(4096)
+#             if not chunk:
+#                 break
+#             file_md5.update(chunk)
+#         file_md5 = file_md5.hexdigest()
+#     file_info = {
+#         'id': str(uuid.uuid4()),
+#         'name': file,
+#         'filesize': human_readable_size(file_size),
+#         'md5sum': file_md5,
+#         'belongs_to_container': "alspacdcs:"
+#     }
+#     progress_queue.put(file_info)
+#     return file_info
+
+
 def process_file(root, file, progress_queue):
     file_path = os.path.join(root, file)
     file_size = os.path.getsize(file_path)
@@ -16,14 +38,19 @@ def process_file(root, file, progress_queue):
             file_md5.update(chunk)
         file_md5 = file_md5.hexdigest()
     file_info = {
-        'id': str(uuid.uuid4()),
-        'name': file,
-        'filesize': human_readable_size(file_size),
-        'md5sum': file_md5,
-        'belongs_to_container': "alspacdcs:1f00f76e-e198-4367-b87a-55ca74378246"
+        'id': f'alspacdcs:{uuid.uuid4()}',
+        'name': os.path.splitext(file)[0],
+        'data_distributions': [{
+            'id': f'alspacdcs:{uuid.uuid4()}',
+            'name': file,
+            'md5sum': file_md5,
+            'filesize': human_readable_size(file_size),
+            'filetype': os.path.splitext(file)[1]
+        }]
     }
     progress_queue.put(file_info)
     return file_info
+
 
 def human_readable_size(size, decimal_places=1):
     for unit in ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB']:
@@ -44,7 +71,7 @@ def get_file_info(directory):
             count += len(results)
             while not progress_queue.empty():
                 progress_file = progress_queue.get()
-                print(f'Processed {progress_file["filename"]}')
+                #print(f'Processed {progress_file["filename"]}')
         print(f'Processed {count} of {total_files} files')
     return file_info
 
@@ -54,6 +81,6 @@ def write_to_yaml(file_info, yaml_file):
 
 #        yaml.dump(file_info, f, default_flow_style=False, allow_unicode=True, sort_keys=['id', 'filename', 'file_size', 'md5sum', 'belongs_to_container'])
 
-directory = '/mnt/storage/private/alspacdata/datasets/dataset_cnv_550_g1/released/2015-11-09/data/raw/'
+directory = '/mnt/storage/private/alspacdata/datasets/dataset_gi_1000g_g0p/released/2016-11-22/data/directly_genotyped/grm/'
 file_info = get_file_info(directory)
-write_to_yaml(file_info, 'file_info_raw_cnv.yaml')
+write_to_yaml(file_info, 'grm.yaml')
